3083084708278cb410f9f420ba221084be7ce764
Hung Do <hungdojan@gmail.com>
2023-10-25 07:55:07 +0200

    influxdb

---
+++ Part1/influx/data/query.sql
@@ -0,0 +1,9 @@
+USE "upa-bucket"
+precision RFC3339
+SELECT * FROM "co_8h"
+SELECT * FROM "no2_1h"
+SELECT * FROM "o3_1h"
+SELECT * FROM "pm10_1h"
+SELECT * FROM "pm10_24h"
+SELECT * FROM "pm2_5_1h"
+SELECT * FROM "so2_1h"
---
+++ Part1/influx/run_db_v1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ $# -lt 1 ]]; then
+    podman run -d --rm \
+        -p 8087:8086 \
+        -v influx_db_volume:/var/lib/influxdb2 \
+        -v ./influx-configs:/etc/influxdb2/influx-configs \
+        -v ./data:/data \
+        --name influxdb_v1 \
+        influxdb:1.8-alpine
+elif [[ $1x == '-ex' ]]; then
+    podman exec -it influxdb_v1 bash
+elif [[ $1x == '-kx' ]]; then
+    podman kill influxdb_v1
+fi
+# influx write -b upa-bucket -f /data/air_quality_formatted.csv --header "#datatype measurement,double,double,double,ignore,tag,string,tag,ignore,ignore,dateTime:2006-01-02 15:04:05-07:00,ignore
