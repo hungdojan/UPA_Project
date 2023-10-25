643928b813b9e4501109d00d37a231f5c5cb1fee
Hung Do <hungdojan@gmail.com>
2023-10-26 00:25:25 +0200

    InfluxQL

--- Part1/influx/run_db_v1.sh
+++ Part1/influx/run_db_v1.sh
@@ -3,9 +3,9 @@
 if [[ $# -lt 1 ]]; then
     podman run -d --rm \
         -p 8087:8086 \
-        -v influx_db_volume:/var/lib/influxdb2 \
-        -v ./influx-configs:/etc/influxdb2/influx-configs \
-        -v ./data:/data \
+        -v influx_db_volume_v1:/var/lib/influxdb2:Z \
+        -v ./influx-configs:/etc/influxdb2/influx-configs:Z \
+        -v ./data:/data:Z \
         --name influxdb_v1 \
         influxdb:1.8-alpine
 elif [[ $1x == '-ex' ]]; then
