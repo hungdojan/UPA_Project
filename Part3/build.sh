632b32ea2b3326966de951ad81016481ddefde66
Hung Do <hungdojan@gmail.com>
2023-11-27 22:18:48 +0100

    Build script fix

--- Part3/build.sh
+++ Part3/build.sh
@@ -2,7 +2,8 @@
 
 if [ ! -d ".venv" ]; then
     python3.10 -m venv .venv
+    source .venv/bin/activate
     if [ -f "requirements.txt" ]; then
-        pip3.10 install -r requirements.txt
+        pip3.10 install --no-cache-dir -r requirements.txt
     fi
 fi
